0c5b71e364c9a388b9be979c1cd71651bf15f137
NazShkh <nazneen.s@datamato.com>
2018-09-12 12:52:36 +0530

    commit1_BSCS

---
+++ Test_Jenkins_BSCS/src/EvenNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a = sc.nextInt();
+		if(a%2 == 0)
+			System.out.println(a+" is an Even number");
+		else
+			System.out.println(a+" is a Odd number");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
